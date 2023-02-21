7fdfa093caba4ff308188d212e15af28ae092fae
Alexis Charpentier <alexis.charpentier_ext@sonepar.com>
2023-02-21 22:06:32 +0100

    readme + default locale fallback + json response

--- src/service.ts
+++ src/service.ts
@@ -2,14 +2,14 @@ import frData from './assets/french-formated.json';
 import enData from './assets/english-formated.json';
 import { Locale } from './types';
 
-export const getWord = (locale: Locale): string => {
+export const getWord = (locale: Locale): any => {
   if (locale === 'fr-FR') {
     const randomIndex = Math.floor(Math.random() * (frData.length + 1));
-    return frData[randomIndex];
+    return { word: frData[randomIndex] };
   }
-  if (locale === 'en-GB') {
+  if (locale === 'en-GB' || !locale) {
     const randomIndex = Math.floor(Math.random() * (enData.length + 1));
-    return enData[randomIndex];
+    return { word: enData[randomIndex] };
   }
-  return "This locale isn't available";
+  return { error: 'The locale you provided does not exist' };
 };
