456b97076934ad4bbcf336ecc7b381648c5750cf
Alexis Charp <alexischarp@gmail.com>
2024-03-14 12:26:37 +0100

    fix railway port issue

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,14 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import type { Locale } from './types';
 import { getWord } from './service';
+import { config } from 'dotenv';
+
+if (process.env.NODE_ENV !== 'production') {
+  config();
+}
 
 const app = express();
-const port = Number(process.env.port) || 3000;
+const port = Number(process.env.PORT) || 3333;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,6 +20,7 @@ app.post('/', (req, res) => {
   res.send(getWord(locale));
 });
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
   console.log(`HangmanAPI up and running on port ${port}`);
+  console.log(`Currently in ${process.env.NODE_ENV} mode`);
 });
