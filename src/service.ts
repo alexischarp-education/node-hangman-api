778cde2d5fc5ef8ef3f671711c543e0ee5ae8061
Alexis Charp <alexischarp@gmail.com>
2024-03-14 12:57:27 +0100

    fix array index issue

--- src/service.ts
+++ src/service.ts
@@ -4,11 +4,11 @@ import { Locale } from './types';
 
 export const getWord = (locale: Locale): any => {
   if (locale === 'fr-FR') {
-    const randomIndex = Math.floor(Math.random() * (frData.length + 1));
+    const randomIndex = Math.floor(Math.random() * frData.length);
     return { word: frData[randomIndex] };
   }
   if (locale === 'en-GB' || !locale) {
-    const randomIndex = Math.floor(Math.random() * (enData.length + 1));
+    const randomIndex = Math.floor(Math.random() * enData.length);
     return { word: enData[randomIndex] };
   }
   return { error: 'The locale you provided does not exist' };
